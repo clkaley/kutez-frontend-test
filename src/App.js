25b921c791bbddf10308b34077d5d6f7dccabd0e
Aleyna Celik <clkkaley@gmail.com>
2022-10-28 13:20:01 +0300

    added Products.js file on the App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Card from "./component/Card";
 import Footer from "./component/Footer";
 import Navbar from "./component/Navbar";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Products/>
       <Footer/>
     </>
   );
