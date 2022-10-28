d171abfb81fda471b5ca2c6eaee6055af9dd8cc1
Aleyna Celik <clkkaley@gmail.com>
2022-10-28 15:47:21 +0300

    added ToastContainer and toast

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import Card from "./component/Card";
 import Footer from "./component/Footer";
 import Navbar from "./component/Navbar";
 import Products from "./pages/Products";
-
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
-      <Navbar/>
-      <Products/>
-      <Footer/>
+      <Navbar />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Products />
+      <Footer />
     </>
   );
 }
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,12 +1,36 @@
 import React from "react";
 import "./style.css";
 import { FaShoppingBasket, FaRegCreditCard } from "react-icons/fa";
-
+import { toast } from "react-toastify";
 function Card(props) {
   console.log(props);
+
+  const notifyBasket = () =>
+    toast("Added Basket!", {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+
+    const notifyCheckout = () =>
+    toast("Go to Checkout Process!", {
+      position: "bottom-center",
+      autoClose: 400,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
   return (
     <>
-      <div className="col-lg-4 col-md-6 col-sm-12 mt-4">
+      <div className="col-lg-4 col-xl-3 col-md-6 col-sm-6  mt-4">
         <div className="card">
           <div className="card-body">
             <img className="card-img-top" src={props.img} alt="" />
@@ -26,13 +50,15 @@ function Card(props) {
               </div>
               <h4 className="price mt-3">{props.price} $</h4>
               <div className="flex-container">
-                <button className="button">
+                <button className="button" onClick={notifyBasket}>
                   Add Basket{" "}
                   <FaShoppingBasket
                     style={{ marginBottom: "2px", marginLeft: "10px" }}
                   />
                 </button>
-                <button className="button">
+
+                <button className="button"
+                onClick={notifyCheckout}>
                   Checkout{" "}
                   <FaRegCreditCard
                     style={{ marginBottom: "2px", marginLeft: "10px" }}
